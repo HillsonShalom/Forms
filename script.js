d4df7579018e5404c9398afc6e47697d1aa895fd
BennyNoyman <bn0504127633@gmail.com>
2024-09-11 14:54:29 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// 1
+function filterAndSortEvenNumbers(arr){
+    return arr;
+}
+
+// 2
+function removeDuplicates(arr){
+    return arr;
+}
+
+// 3
+function capitalizeFirstLetter(str){
+    return str;
+}
+
+// 4
+function saveTasks(){
+
+}
+function loadTasks(){
+
+}
+function addTask(){
+
+}
+function removeTask(){
+    
+}
