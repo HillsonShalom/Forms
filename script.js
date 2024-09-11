9e9a2e00bec541c83e8147fef34a618b84b4ee18
BennyNoyman <bn0504127633@gmail.com>
2024-09-11 15:12:01 +0300

    complete 2

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 // 1
-function filterAndSortEvenNumbers(arr){
+function filterAndSortEvenNumbers(arr = []){
     arr = arr.filter(e => e % 2 === 0);
     arr.sort((a, b) => a - b);
     return arr;
 }
 
 // 2
-function removeDuplicates(arr){
+function removeDuplicates(arr = []){
+    arr.sort((a, b) => a -b);
+    let num;
+    arr = arr.filter(n => {if (n !== num){ num = n; return n;}});
     return arr;
 }
 
