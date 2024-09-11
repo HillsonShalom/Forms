b669cd73249a08593377733f789cd1fdb772283e
BennyNoyman <bn0504127633@gmail.com>
2024-09-11 14:57:18 +0300

    complete 1

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // 1
 function filterAndSortEvenNumbers(arr){
+    arr = arr.filter(e => e % 2 === 0);
+    arr.sort((a, b) => a - b);
     return arr;
 }
 
@@ -24,5 +26,5 @@ function addTask(){
 
 }
 function removeTask(){
-    
+
 }
